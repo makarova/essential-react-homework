5b2903e3c7f6e8ea487544ad8de2ce92a090a87d
viktoriia.kucherenko <viktoriia.kucherenko@getyourguide.com>
2026-05-10 17:26:55 +0200

    add integration test for home

--- mobile/jest.config.cjs
+++ mobile/jest.config.cjs
@@ -1,13 +1,8 @@
 module.exports = {
   preset: 'jest-expo',
-  testEnvironment: 'node',
-  transformIgnorePatterns: [
-    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)',
-  ],
-  collectCoverageFrom: [
-    'src/**/*.{ts,tsx}',
-    '!src/**/*.d.ts',
-    '!src/**/__tests__/**',
-  ],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  testEnvironment: 'jsdom',
+  watchman: false,
+  moduleNameMapper: {
+    '^@expo/vector-icons$': '<rootDir>/test/mocks/expoVectorIcons.js',
+  },
 };
