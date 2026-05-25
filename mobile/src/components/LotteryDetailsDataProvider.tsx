17906b68111d66f306a9689ee6f207cc30418727
viktoriia.kucherenko <viktoriia.kucherenko@getyourguide.com>
2026-05-25 19:07:08 +0200

    fix after review

--- mobile/src/components/LotteryDetailsDataProvider.tsx
+++ mobile/src/components/LotteryDetailsDataProvider.tsx
@@ -2,6 +2,7 @@ import { ReactElement } from 'react';
 import { useLotteryDetails } from '../hooks/useLotteryDetails';
 import { Lottery } from '../types';
 import { Loader } from './Loader';
+import LotteryDetailsError from './LotteryDetailsError';
 
 interface LotteryDetailsDataProviderProps {
   children: (lotteryDetails: Lottery) => ReactElement;
@@ -12,9 +13,10 @@ const LotteryDetailsDataProvider = ({
   children,
   lotteryId,
 }: LotteryDetailsDataProviderProps) => {
-  const { data, loading } = useLotteryDetails(lotteryId);
+  const { data, loading, error } = useLotteryDetails(lotteryId);
 
   if (loading) return <Loader />;
+  if (error) return <LotteryDetailsError />;
   return data ? children(data) : null;
 };
 
